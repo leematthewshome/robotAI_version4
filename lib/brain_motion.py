181d1db2f520f8da5fa528a5e2a99b4cdb995450
leematthewshome <lee.matthews.home@gmail.com>
2020-12-24 11:44:37 +1100

    Modify brain files to be object libraries

--- lib/brain_motion.py
+++ lib/brain_motion.py
@@ -14,47 +14,58 @@ import base64
 import pika
 from datetime import datetime
 
+
 #-------------------------------------------------------------------------------------------------------------------------
-# Object Detection Detector
+# Object Detection detector
 #-------------------------------------------------------------------------------------------------------------------------
 class detectorAPI:
 
-    def __init__(self, logger, topDir):
+    def __init__(self, ENVIRON):
+        debugOn = True
+
+        # setup logging based on level
+        logging.basicConfig()
+        logger = logging.getLogger("brain_motion")
+        if debugOn:
+            logger.level = logging.DEBUG
+        else:
+            logger.level = logging.INFO
         self.logger = logger
+        
+        self.ENVIRON = ENVIRON
+
         # filepath parameters parameters
-        self.model_path = os.path.join(topDir, "static/MLModels/object/MobileNetSSD_deploy.caffemodel")
-        self.proto_path = os.path.join(topDir, "static/MLModels/object/MobileNetSSD_deploy.prototxt.txt")
-        self.conf_cutoff = 0.5
+        obj_model_path = os.path.join(ENVIRON["topdir"], "static/MLModels/object/MobileNetSSD_deploy.caffemodel")
+        obj_proto_path = os.path.join(ENVIRON["topdir"], "static/MLModels/object/MobileNetSSD_deploy.prototxt.txt")
+        self.obj_conf_cutoff = 0.5
 
         # initialize the list of class labels for detection
         self.CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
                 "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-        print("loading dnn")
-        self.net = cv2.dnn.readNetFromCaffe(self.proto_path, self.model_path)
+        self.obj_net = cv2.dnn.readNetFromCaffe(obj_proto_path, obj_model_path)
 
 
+    # function to detect objects
+    #-----------------------------------------------------------------------
     def objectCount(self, imgbin):
         frame = cv2.imdecode(np.frombuffer(imgbin, np.uint8), -1)
-        #frame = imutils.resize(frame, width=400)
-        # grab the frame dimensions and convert it to a blob
-        #(h, w) = frame.shape[:2]
-        #print("converting to blob")
         blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
         # pass the blob through the network and obtain the detections and predictions
-        #print("getting objects")
-        self.net.setInput(blob)
-        detections = self.net.forward()
+        self.obj_net.setInput(blob)
+        self.logger.debug('Running the model for object detections')
+        detections = self.obj_net.forward()
 
         # loop over the detections
         dictObjects = {}
         for i in np.arange(0, detections.shape[2]):
             # filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
             confidence = detections[0, 0, i, 2]
-            if confidence > self.conf_cutoff:
+            if confidence > self.obj_conf_cutoff:
                 # extract the index of the class label from the `detections`
                 idx = int(detections[0, 0, i, 1])
                 className = self.CLASSES[idx]
-                        #check if there is already an entry in dictionary
+                self.logger.debug(className + " detected")
+                #check if there is already an entry in dictionary
                 if className in dictObjects:
                     dictObjects[className] = dictObjects[className] + 1
                 else:
@@ -64,57 +75,40 @@ class detectorAPI:
         return dictObjects
 
 
-#---------------------------------------------------------------------------
-# Function called by robotAI_brain for this set of logic
-#---------------------------------------------------------------------------
-def doLogic(ENVIRON, msgQueue, content, reply_to, body):
+    # Function called by robotAI_brain for this set of logic
+    #-----------------------------------------------------------------------
+    def doLogic(self, msgQueue, content, reply_to, body):
+        # If we received an image then check it for objects
+        if content == "image/jpg":
+            self.logger.debug('Decode the content and save the file')
+            imgbin = base64.b64decode(body)
+	
+            # Overwrite current image stored for client
+            # -------------------------------------------
+            filePath = os.path.join(self.ENVIRON['topdir'], 'static/motionImages', reply_to + '.jpg') 
+            with open(filePath, 'wb') as f_output:
+                f_output.write(imgbin)
+		
+            # Store image in history folder for client
+            # ----------------------------------------       
+            folder = os.path.join(self.ENVIRON['topdir'], 'static/motionImages/', reply_to) 
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            filePath = os.path.join(folder, datetime.now().strftime("%Y%m%d%H%M%S") + '.jpg') 
+            with open(filePath, 'wb') as f_output:
+                f_output.write(imgbin)
+            self.logger.debug("Saved image to " + filePath )
 
-    debugOn = True
-
-    # setup logging using the python logging library
-    #-----------------------------------------------------
-    logging.basicConfig()
-    logger = logging.getLogger("brain_motion")
-    if debugOn:
-        logger.level = logging.DEBUG
-    else:
-        logger.level = logging.INFO
-
-    # If we received an image then check it for objects
-    if content == "image/jpg":
-        logger.debug('Decode the content and save the file')
-        imgbin = base64.b64decode(body)
-        
-        # Overwrite current image stored for client
-        # -------------------------------------------
-        filePath = os.path.join(ENVIRON['topdir'], 'static/motionImages', reply_to + '.jpg') 
-        with open(filePath, 'wb') as f_output:
-            f_output.write(imgbin)
-        
-        # Store image in history folder for client
-        # ----------------------------------------       
-        folder = os.path.join(ENVIRON['topdir'], 'static/motionImages/', reply_to) 
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        filePath = os.path.join(folder, datetime.now().strftime("%Y%m%d%H%M%S") + '.jpg') 
-        with open(filePath, 'wb') as f_output:
-            f_output.write(imgbin)
-        logger.debug("Saved image to " + filePath )
-
-        # use Machine learning to determine if a person exists in the image
-        # -----------------------------------------------------------------
-        logger.debug('Analysing the image using detectorAPI class')
-        dt = detectorAPI(logger, ENVIRON['topdir'])
-        logger.debug('Instantiated detectorAPI class, running objectCount...')
-        result = dt.objectCount(imgbin)
-        # respond to the client device that submitted the message
-        body = json.dumps(result)
-        logger.debug('About to send data: ' + body)
-        logger.debug('Sending data to: ' + reply_to)
-        channel1 = msgQueue.channel()
-        channel1.queue_declare(reply_to)
-        properties = pika.BasicProperties(app_id='motion', content_type='application/json', reply_to='Central')
-        channel1.basic_publish(exchange='', routing_key=reply_to, body=body, properties=properties)
-
+            # use Machine learning to determine if a person exists in the image
+            # -----------------------------------------------------------------
+            self.logger.debug('Analysing the image using detectorAPI class')
+            result = self.objectCount(imgbin)
+            # respond to the client device that submitted the message
+            body = json.dumps(result)
+            self.logger.debug('Sending data to: ' + reply_to + '. body = ' + body)
+            channel1 = msgQueue.channel()
+            channel1.queue_declare(reply_to)
+            properties = pika.BasicProperties(app_id='motion', content_type='application/json', reply_to='Central')
+            channel1.basic_publish(exchange='', routing_key=reply_to, body=body, properties=properties)
 
 
--- lib/brain_voice.py
+++ lib/brain_voice.py
@@ -12,201 +12,268 @@ import pika
 import os
 
 # imports for the ML Chatbot
+import json 
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-# create connection to database
-# ----------------------------------------------------------------------------------
-def createConn(topdir):
-    try:
-        dbpath = os.path.join(topdir, 'static/db/robotAI.db')
-        conn = sqlite3.connect(dbpath)
-    except:
-        return None
-    return conn
+from tensorflow.keras.models import load_model
 
 
-# Send details to the message queue
-# ----------------------------------------------------------------------------------
-def sendMessage(logger, ENVIRON, reply_to, body):
-    try:
-        credentials = pika.PlainCredentials(ENVIRON["queueUser"], ENVIRON["queuePass"])
-        parameters = pika.ConnectionParameters(ENVIRON["queueSrvr"], ENVIRON["queuePort"], '/',  credentials)
-        connection = pika.BlockingConnection(parameters)
-        channel1 = connection.channel()
-        channel1.queue_declare(reply_to)
-        properties = pika.BasicProperties(app_id='voice', content_type='application/json', reply_to=ENVIRON["brainQueue"])
-        channel1.basic_publish(exchange='', routing_key=reply_to, body=body, properties=properties)
-        connection.close()
-    except:
-        logger.error('There was an error sending the message to the queue')
-        return False
-    return True
+#-------------------------------------------------------------------------------------------------------------------------
+# Object Detection detector
+#-------------------------------------------------------------------------------------------------------------------------
+class voiceAPI:
 
+    def __init__(self, ENVIRON):
+        debugOn = True
 
-# Build JSON format to be sent to the message queue
-#----------------------------------------------------------------------------------
-def buildMessage(logger, response):
-    result = {'action': 'chat', 'list': response}
-    body = json.dumps(result)
-    result = sendMessage(logger, ENVIRON, reply_to, body)
-    return body
-
-
-# ---------------------------------------------------------------------------------
-# Fetch a list of statements from the chat bot table
-#----------------------------------------------------------------------------------
-def getChatPath(topdir, logger, chatid='0-GREETA-0'):
-    # make 2 part chat IDs match the way 'next' column in DB formatted
-    arr = chatid.split('-')
-    if len(arr) == 2:
-        chatid = '0-' + chatid
-    logger.debug('Running function getChatPath with chatid: ' + chatid)
-
-    # connect to database and check for valid subscription
-    conn = createConn(topdir)
-    if conn is None:
-        logger.error('Could not connect to database. Exiting function.')
-        return {}
-    else:
-        cur = conn.cursor()
-        logger.debug('Connection to DB established and ready with cursor.')
-
-    # function to build the SQL stmnt
-    def buildSQL(table, sCat, iItm):
-        SQL = "SELECT text, funct, next FROM " + table + " WHERE Category = '" + sCat + "'"
-        if str(iItm) != '0':
-            SQL += " AND Item = " + str(iItm)
+        # setup logging based on level
+        logging.basicConfig()
+        logger = logging.getLogger("brain_voice")
+        if debugOn:
+            logger.level = logging.DEBUG
         else:
-            SQL += " ORDER BY RANDOM() LIMIT 1 ;"
-        return SQL
-
-    # recursively loop through database to get path
-    def chatLoop(chatid):
-        list = []
-        d = None
-        row = chatid.split("-")
-        sCat = row[1]
-        iItm = row[2]
-
-        # create SQL query (add multiple languages later)
-        SQL = buildSQL('ChatText', sCat, iItm)
-        try:
-            cur.execute(SQL)
-            list = cur.fetchall()
-        except:
-            conn.rollback()
-
-        # insert result into the json data object
-        if len(list) > 0:
-            d = {'text': list[0][0],
-               'funct': list[0][1],
-               'next': list[0][2]}
-        else:
-            d = {'text': 'Something went wrong. I could not find the requested chat entry ', 'funct': '', 'next': ''}
-        return d
-
-    # run the chat loop to build up our chat list and then return result
-    chatlst = []
-    while '|' not in chatid and len(chatid) > 0:
-        row = chatLoop(chatid)
-        chatlst.append(row)
-        try:
-            chatid = row['next']
-        except:
-            chatid = ''
-
-    conn.close()
-
-    return chatlst
-
-
-
-#---------------------------------------------------------------------------
-# Function called by robotAI_brain for this set of logic
-#---------------------------------------------------------------------------
-def doLogic(ENVIRON, content, reply_to, body, chatmodel, chatdata, tokenizer, encoder):
-    debugOn = True
-    action = ""
-    topdir = ENVIRON['topdir']
-
-    # setup logging using the python logging library
-    #-----------------------------------------------------
-    logging.basicConfig()
-    logger = logging.getLogger("brain_voice")
-    if debugOn:
-        logger.level = logging.DEBUG
-    else:
-        logger.level = logging.INFO
-
-
-    # If we received JSON then get the requested action
-    #-----------------------------------------------------
-    if content == 'application/json':
-        logger.debug('JSON received by brain_voice. Decoding...')
-        try:
-            data = json.loads(body.decode("utf-8"))
-            action = data["action"]
-        except:
-            logger.error('Could not load response as JSON or extract key of "type"')
-    elif content == 'audio/wav':
-        logger.info('This is a placeholder for our speech to text functionality. If we ever move to the brain.')
-        action = "stt"
-
-    logger.debug('Action value is: ' + action)
-
-
-    # Execute requested VOICE action to execute
-    #-----------------------------------------------------
-    if action == "getChat":
-        # need to fetch the relevant chat text requested
-        chatid = data["chatItem"]
-        logger.debug('Calling getChatPath function')
-        result = getChatPath(topdir, logger, chatid)
-        # return data to the client device that initiated the request 
-        result = {'action': 'chat', 'list': result}
-        body = json.dumps(result)
-        logger.debug("Sending chat text to : " + reply_to)
-        result = sendMessage(logger, ENVIRON, reply_to, body)
+            logger.level = logging.INFO
+        self.logger = logger
         
-    elif action == "getResponse":
-        # need to get the chat response from the ML Chat model
+        self.ENVIRON = ENVIRON
+
+        # Check for chat database
+        #-----------------------------------------------------
+        dbpath = os.path.join(self.ENVIRON["topdir"], 'static/db/robotAI.db')
+        if not os.path.isfile(dbpath):
+            self.buildDB()
+
+        # cache AI chatbot components to speed things up
+        #--------------------------------------------------
+        vocab_size = 20000
+        embedding_dim = 16
+        oov_token = "<OOV>"
         max_len = 20
         trunc_type = 'post'
-        result = []    
-        text = data["text"]
-        
-        logger.debug('Running prediction for: ' + text)
-        predictions = chatmodel.predict(pad_sequences(tokenizer.texts_to_sequences([text]), truncating=trunc_type, maxlen=max_len))[0]
-        highest = predictions[np.argmax(predictions)]
-        category = encoder.inverse_transform([np.argmax(predictions)]) 
-        logger.debug("MLChatBot found " + str(highest) + " percent match to " + str(category))
-        if highest > .75:
-            for i in chatdata['intents']:
-                if i['tag']==category:
-                    if len(i['context_set']) > 0:
-                        result = getChatPath(topdir, logger, i['context_set'])
-                    else:
-                        response = np.random.choice(i['responses'])
-                        response = {'text': response, 'funct': '', 'next': ''}    
-                        result.append(response)
-        else:
-            response = "Sorry, I dont have a suitable response to that"
-            response = {'text': response, 'funct': '', 'next': ''}    
-            result.append(response)
-            logpath  = os.path.join(topdir, 'static/MLModels/chatbot/unhandled.log')
-            f = open(logpath, "a")
-            f.write(text + "\n")
-            f.close()
+        modelpath = os.path.join(self.ENVIRON["topdir"], 'static/MLModels/chatbot')
+        chatpath  = os.path.join(self.ENVIRON["topdir"], 'static/MLModels/chatbot/chatschema.json')
+        self.chatmodel = load_model(modelpath)
+        with open(chatpath) as file:
+            self.chatdata = json.load(file)
 
-        # return data to the client device that initiated the request 
-        result = {'action': 'chat', 'list': result}
+        training_sentences = []
+        training_labels = []
+        for intent in self.chatdata['intents']:
+            for pattern in intent['patterns']:
+                training_sentences.append(pattern)
+                training_labels.append(intent['tag'])
+
+        # encode our list of tags  
+        self.encoder = LabelEncoder()
+        self.encoder.fit(training_labels)
+        training_labels = self.encoder.transform(training_labels)
+
+        self.tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token) 
+        self.tokenizer.fit_on_texts(training_sentences)
+
+
+    # create connection to database
+    # ----------------------------------------------------------------------------------
+    def createConn(self):
+        try:
+            dbpath = os.path.join(self.ENVIRON["topdir"], 'static/db/robotAI.db')
+            conn = sqlite3.connect(dbpath)
+        except:
+            return None
+        return conn
+
+
+    # build new chat database
+    # ----------------------------------------------------------------------------------
+    def buildDB(self):
+        self.logger.warning("Warning: No ChatText DB found. Creating now... ")
+        import csv
+        conn = self.createConn()
+        # now create tables and populate from CSV
+        cur = conn.cursor() 
+        cur.execute("""CREATE TABLE ChatText (category VARCHAR(32) NOT NULL, item INTEGER NOT NULL, text VARCHAR(255), funct VARCHAR(255), next VARCHAR(255))""")
+        cur.execute("""CREATE INDEX ChatText_cat ON ChatText(category)""")
+        cur.execute("""CREATE INDEX ChatText_catitem ON ChatText(category, item)""")
+        csvpath = os.path.join(self.ENVIRON["topdir"], 'static/db/ChatText.csv')
+        with open(csvpath, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                data = (row['category'], row['item'], row['text'], row['funct'], row['next'])
+                cur.execute("INSERT INTO ChatText VALUES (?,?,?,?,?);", data)
+        conn.commit()
+        self.logger.warning("ChatText DB was successfully created from CSV. ")
+        conn.close()
+
+
+    # Send details to the message queue
+    # ----------------------------------------------------------------------------------
+    def sendMessage(self, reply_to, body):
+        try:
+            credentials = pika.PlainCredentials(self.ENVIRON["queueUser"], self.ENVIRON["queuePass"])
+            parameters = pika.ConnectionParameters(self.ENVIRON["queueSrvr"], self.ENVIRON["queuePort"], '/',  credentials)
+            connection = pika.BlockingConnection(parameters)
+            channel1 = connection.channel()
+            channel1.queue_declare(reply_to)
+            properties = pika.BasicProperties(app_id='voice', content_type='application/json', reply_to=self.ENVIRON["brainQueue"])
+            channel1.basic_publish(exchange='', routing_key=reply_to, body=body, properties=properties)
+            connection.close()
+        except:
+            self.logger.error('There was an error sending the message to the queue')
+            return False
+        return True
+
+
+    # Build JSON format to be sent to the message queue
+    #----------------------------------------------------------------------------------
+    def buildMessage(self, response):
+        result = {'action': 'chat', 'list': response}
         body = json.dumps(result)
-        logger.debug("Sending chat text to : " + reply_to)
-        result = sendMessage(logger, ENVIRON, reply_to, body)
-    else:
-        # catch all if we didnt expect the action or was blank
-        logger.warning('The action value of ' + action + ' has no code to handle it.')
+        result = self.sendMessage(reply_to, body)
+        return body
+
+
+    # ---------------------------------------------------------------------------------
+    # Fetch a list of statements from the chat bot table
+    #----------------------------------------------------------------------------------
+    def getChatPath(self, chatid='0-GREETA-0'):
+        # make 2 part chat IDs match the way 'next' column in DB formatted
+        arr = chatid.split('-')
+        if len(arr) == 2:
+            chatid = '0-' + chatid
+        self.logger.debug('Running function getChatPath with chatid: ' + chatid)
+
+        # connect to database and check for valid subscription
+        conn = self.createConn()
+        if conn is None:
+            self.logger.error('Could not connect to database. Exiting function.')
+            return {}
+        else:
+            cur = conn.cursor()
+
+        # function to build the SQL stmnt
+        def buildSQL(table, sCat, iItm):
+            SQL = "SELECT text, funct, next FROM " + table + " WHERE Category = '" + sCat + "'"
+            if str(iItm) != '0':
+                SQL += " AND Item = " + str(iItm)
+            else:
+                SQL += " ORDER BY RANDOM() LIMIT 1 ;"
+            return SQL
+
+        # recursively loop through database to get path
+        def chatLoop(chatid):
+            list = []
+            d = None
+            row = chatid.split("-")
+            sCat = row[1]
+            iItm = row[2]
+
+            # create SQL query (add multiple languages later)
+            SQL = buildSQL('ChatText', sCat, iItm)
+            try:
+                cur.execute(SQL)
+                list = cur.fetchall()
+            except:
+                conn.rollback()
+
+            # insert result into the json data object
+            if len(list) > 0:
+                d = {'text': list[0][0],
+                   'funct': list[0][1],
+                   'next': list[0][2]}
+            else:
+                d = {'text': 'Something went wrong. I could not find the requested chat entry ', 'funct': '', 'next': ''}
+            return d
+
+        # run the chat loop to build up our chat list and then return result
+        chatlst = []
+        while '|' not in chatid and len(chatid) > 0:
+            row = chatLoop(chatid)
+            chatlst.append(row)
+            try:
+                chatid = row['next']
+            except:
+                chatid = ''
+
+        conn.close()
+        return chatlst
+
+
+
+    #---------------------------------------------------------------------------
+    # Function called by robotAI_brain for this set of logic
+    #---------------------------------------------------------------------------
+    def doLogic(self, content, reply_to, body):
+        debugOn = True
+        action = ""
+
+        # If we received JSON then get the requested action
+        #-----------------------------------------------------
+        if content == 'application/json':
+            self.logger.debug('JSON received by brain_voice. Decoding...')
+            try:
+                data = json.loads(body.decode("utf-8"))
+                action = data["action"]
+            except:
+                self.logger.error('Could not load response as JSON or extract key of "type"')
+        elif content == 'audio/wav':
+            self.logger.info('This is a placeholder for our speech to text functionality. If we ever move to the brain.')
+            action = "stt"
+        self.logger.debug('Action value is: ' + action)
+
+
+        # Execute requested VOICE action to execute
+        #-----------------------------------------------------
+        if action == "getChat":
+            # need to fetch the relevant chat text requested
+            chatid = data["chatItem"]
+            self.logger.debug('Calling getChatPath function for ' + chatid)
+            result = self.getChatPath(chatid)
+            # return data to the client device that initiated the request 
+            result = {'action': 'chat', 'list': result}
+            body = json.dumps(result)
+            self.logger.debug("Sending chat text to : " + reply_to)
+            result = self.sendMessage(reply_to, body)
+        
+        elif action == "getResponse":
+            # need to get the chat response from the ML Chat model
+            max_len = 20
+            trunc_type = 'post'
+            result = []    
+            text = data["text"]
+        
+            self.logger.debug('Running prediction for: ' + text)
+            predictions = self.chatmodel.predict(pad_sequences(self.tokenizer.texts_to_sequences([text]), truncating=trunc_type, maxlen=max_len))[0]
+            highest = predictions[np.argmax(predictions)]
+            category = self.encoder.inverse_transform([np.argmax(predictions)]) 
+            self.logger.debug("MLChatBot found " + str(highest) + " percent match to " + str(category))
+            if highest > .75:
+                for i in self.chatdata['intents']:
+                    if i['tag']==category:
+                        if len(i['context_set']) > 0:
+                            result = self.getChatPath(i['context_set'])
+                        else:
+                            response = np.random.choice(i['responses'])
+                            response = {'text': response, 'funct': '', 'next': ''}    
+                            result.append(response)
+            else:
+                response = "Sorry, I dont have a suitable response to that"
+                response = {'text': response, 'funct': '', 'next': ''}    
+                result.append(response)
+                logpath  = os.path.join(self.ENVIRON["topdir"], 'static/MLModels/chatbot/unhandled.log')
+                f = open(logpath, "a")
+                f.write(text + "\n")
+                f.close()
+
+            # return data to the client device that initiated the request 
+            result = {'action': 'chat', 'list': result}
+            body = json.dumps(result)
+            self.logger.debug("Sending chat text to : " + reply_to)
+            result = self.sendMessage(reply_to, body)
+        else:
+            # catch all if we didnt expect the action or was blank
+            self.logger.warning('The action value of ' + action + ' has no code to handle it.')
 
 
 
