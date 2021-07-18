458b2320b4e72cf582620f8d8e350ae668a56155
leematthewshome <lee.matthews.home@gmail.com>
2021-07-18 14:22:08 +1000

    latest

--- camFeeds.py
+++ camFeeds.py
@@ -4,11 +4,10 @@ import os
 
 def runWeb(ENVIRON):
 
-    #print(ENVIRON['topdir'])
-    imagepath = os.path.join(ENVIRON['topdir'], 'static/web')
-    #print(imagepath)
-    #app = Flask(__name__, static_url_path=imagepath)
+
+    imagepath = os.path.join(ENVIRON['topdir'], 'static/motionImages')
     app = Flask(__name__)
+
 
     #Display list of images
     #==========================================================================================
@@ -17,9 +16,32 @@ def runWeb(ENVIRON):
         image_names = os.listdir(imagepath)
         images = ''
         for name in image_names:
-            images += '<div style="text-align: center;"><img src="static/web/' + name + '" > <br><br></div>'
-            html = '<HTML><HEAD></HEAD><BODY><meta http-equiv="refresh" content="1" />' + images + '</BODY></HTML>'
+            images += '<div style="text-align: center;"><img src="static/motionImages/' + name + '" > <br><br></div>'
+            html = '<HTML><HEAD></HEAD><BODY><meta http-equiv="refresh" content="3" />' + images + '</BODY></HTML>'
         return html
+
+    @app.after_request
+    def add_header(r):
+        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        r.headers["Pragma"] = "no-cache"
+        r.headers["Expires"] = "0"
+        r.headers['Cache-Control'] = 'public, max-age=0'
+        return r
        
-    app.run()
+
+    app.run(host= '0.0.0.0')
+
+
+
+# **************************************************************************
+# This will only be executed when we run the sensor on its own for debugging
+# **************************************************************************
+if __name__ == "__main__":
+    import os
+    
+    ENVIRON = {}
+    ENVIRON["topdir"] = os.path.dirname(__file__)
+
+    runWeb(ENVIRON)
+
 
